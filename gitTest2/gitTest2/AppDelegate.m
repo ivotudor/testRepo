4afd8e35adf3a673731dd670a4be7c4ca41e5166
itudorwe <itudorwe@gmail.com>
2014-05-22 11:20:11 +0200

    Another comment!

--- gitTest2/gitTest2/AppDelegate.m
+++ gitTest2/gitTest2/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"Changed some lines");
+    NSLog(@"Line removed now");
     
     return YES;
 }
