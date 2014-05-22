6a3489de931d18a0ddb69d570cd141077c3130c7
itudorwe <itudorwe@gmail.com>
2014-05-22 11:17:45 +0200

    Added line to appDelegate.

--- gitTest2/gitTest2/AppDelegate.m
+++ gitTest2/gitTest2/AppDelegate.m
@@ -13,6 +13,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"Changed some lines");
+    
     return YES;
 }
 							
